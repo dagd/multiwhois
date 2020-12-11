5b00fcbf1888d0446b13d8f36022ec3c1922119d
Rick Elrod <rick@elrod.me>
2020-12-11 00:59:15 -0600

    rename targets to avoid clashes, but default to the renamed ones
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- impls/def.mk
+++ impls/def.mk
@@ -5,14 +5,20 @@ all: fetch patch build
 
 .PHONY: all
 
-fetch:
+fetch-default:
 	curl -Lo whois.c "$(shell cat client.url)"
 
-patch:
+patch-default:
 	patch --no-backup-if-mismatch < client.patch
 
-build:
+build-default:
 	$(CC) whois.c -o whois-$(CWD)
 
-clean:
+clean-default:
 	rm -v whois-$(CWD) whois.c
+
+check-default:
+	./whois-$(CWD) google.com 2>&1 | grep -q markmonitor
+
+%: %-default
+	@true
